5b47344c0c8ea5d5de3d421d2f883c2f1a543143
sidharth-k-2004 <sidharthkdinesan123@gmail.com>
2026-07-30 08:18:38 +0000

    removed check

--- src/main/java/com/neueda/restapi/EmployeeController.java
+++ src/main/java/com/neueda/restapi/EmployeeController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import com.neueda.restapi.Check;
 
 @RestController
 @RequestMapping("/api/v1/employee")
@@ -125,14 +124,7 @@ public class EmployeeController {
     }
 
     @GetMapping("/check")
-    public ResponseEntity<Map<String, Object>> check() {
-        Check check = new Check();
-        String checkResult = check.getCheck();
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("message", "Check endpoint called successfully");
-        response.put("data", checkResult);
-
-        return ResponseEntity.ok(response);
+    public String check() {
+        return "hiii this is check";
     }
 }
